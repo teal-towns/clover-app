02a7823e585b61ef9452ed8e76cdedebd5f0dc91
Luke Madera <luke.madera@gmail.com>
2023-03-28 13:10:45 -0700

    init

---
+++ frontend/lib/modules/home.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import '../app_scaffold.dart';
+
+class HomeComponent extends StatefulWidget {
+  @override
+  _HomeComponentState createState() => _HomeComponentState();
+}
+
+class _HomeComponentState extends State<HomeComponent> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppScaffoldComponent(
+      body: ListView(
+        children: [
+          Text('Home'),
+        ]
+      )
+    );
+  }
+}
